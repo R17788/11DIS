0164d45c04d9ef6b92d62e920a3e4c39a1cbb839
R17788 <17788@stpauls.qld.edu.au>
2022-04-01 10:40:33 +1000

    Unit 1

--- FlaskApp1/__init__.py
+++ FlaskApp1/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from confic import Config
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = MongoEngine
+db.init_app(app)
 
 from FlaskApp1 import routes
