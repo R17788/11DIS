6bdaeb6ff57ac1f07ea24bf51a50d3c0f013c6a9
R17788 <17788@stpauls.qld.edu.au>
2022-05-10 14:56:40 +1000

    Unit 1

--- FlaskApp1/routes.py
+++ FlaskApp1/routes.py
@@ -1,5 +1,7 @@
-from FlaskApp1 import app
-from flask import render_template
+from FlaskApp1 import app, db
+from flask import render_template, request, redirect, flash
+from FlaskApp1.models import User, Course, Enrolments
+from FlaskApp1.forms import LoginForm, RegisterForm
 
 
 @app.route("/")
@@ -10,9 +12,20 @@ def index():
     return render_template("index.html", login=False, index=True)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", login=True)
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.objects(email=email).first()
+        if user and user.get_password(password) == user.password:
+            flash(f"{user.first_name}, you are successfully logged in!")
+            return redirect("/index")
+        else:
+            flash("Sorry, Something went wrong.")
+    return render_template("login.html", title="Login", form=form, login=True)
 
 
 @app.route("/courses")
@@ -31,9 +44,44 @@ def courses():
     return render_template("courses.html", courseData=courseData, courses=True)
 
 
-@app.route("/enrolment", methods=['GET, "POST'])
+@app.route("/enrolment", methods=['GET', 'POST'])
 def enrolment():
     id = request.form.get('courseID')
     title = request.form.get('title')
     term = request.form.get('term')
-    return render_template("enrolment.html", register=True, data={"id":id, "title": title, "term":term})
+    return render_template("enrolment.html", register=True, data={"id": id, "title": title, "term": term})
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_id = User.objects.count()
+        user_id += 1
+        email = form.email.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        user = User(user_id=user, email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        flash("You are successfully registered!")
+        return redirect(url_for('index'))
+    return render_template("register.html", title="Register", form=form, register=True)
+
+@app.route("/user")
+def user():
+    # User(user_id=1, first_name="Ryan", last_name="Hawcroft", email="ryan@sps.com.au", password="abc1234").save()
+    # User(user_id=2, first_name="Connor", last_name="Lowe", email="connor@sps.com.au", password="password123").save()
+    # Users = User.object.all()
+    return render_template("user.html")
+
+
+@app.route("/calculator")
+def calculator():
+    numbers = [
+        {"num1": 6},
+        {"num2": 4},
+    ]
+    print(numbers)
+    return render_template("calculator.html")
---
+++ FlaskApp1/templates/script.py
@@ -0,0 +1,23 @@
+# num1 extern = document.getElementsByTagName("link")[0].import;
+
+operator = None
+num1 = None
+num2 = None
+
+if operator == "A":
+    print(num1 + num2)
+
+if operator == "S":
+    print(num1 - num2)
+
+if operator == "D":
+    print(num1 / num2)
+
+if operator == "M":
+    print(num1 * num2)
+
+print(10)
+
+var input = document.getElementById("userInput");
+
+var extern
